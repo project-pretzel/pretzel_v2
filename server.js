c1fb0bfddd634ad73d083a828fdff523db837bd5
James Kip <jameskip@me.com>
2017-05-26 16:41:33 -0500

    top 20 trends rendering on landing page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const path = require('path');
 const webpackConfig = require('./webpack.config.js');
+const request = require('request');
 
 const app = express();
 
@@ -19,6 +20,18 @@ app.use(webpackDevMiddleware(compiler, {
   },
   historyApiFallback: true,
 }));
+
+app.get('/trends', (req, res) => {
+  console.log('Requesting trends...');
+  request('https://trends.google.com/trends/hottrends/visualize/internal/data', (error, response) => {
+    if (!error && response.statusCode === 200) {
+      const top20Trends = JSON.parse(response.body).united_states;
+      res.json(top20Trends);
+    } else {
+      res.json(error);
+    }
+  });
+});
 
 const server = app.listen(3000, () => {
   const port = server.address().port;
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import axios from 'axios';
 
 import Navigation from './Navigation.jsx';
-
 
 class Landing extends React.Component {
   constructor() {
     super();
     this.state = {
       // TODO: Implement trends to state
+      trends: [],
     };
   }
 
+  componentWillMount() {
+    axios.get('/trends')
+      .then((response) => {
+        const usTrends = response.data;
+        this.setState({ trends: usTrends });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   render() {
+    const trends = this.state.trends.map((trend, i) =>
+      <li key={i}>{trend}</li>,
+    );
+
     return (
       <MuiThemeProvider>
         <div>
           <Navigation />
-          <h4>Landing page...</h4>
+          <br />
+          {trends}
         </div>
       </MuiThemeProvider>
     );
