fe6e7c13df95e803b66f70bddaf54458dda7809f
James Kip <jameskip@me.com>
2017-06-02 14:41:25 -0500

    rss feed rendering on trend

--- src/components/Rss.jsx
+++ src/components/Rss.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import FlatButton from 'material-ui/FlatButton';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
 
-// TODO: Implement RSS feed
 class Rss extends React.Component {
+
+
   render() {
+    let feed;
+    console.log(this.props.feed);
+    if (Array.isArray(this.props.feed.item)) {
+      feed = this.props.feed.item.map((item, i) => (
+        <Card key={i}>
+          <CardText>
+            {item.title}
+          </CardText>
+        </Card>
+      ));
+    }
+
+
     return (
       <div>
         <h2 className="trend-headline">
           {this.props.selected}
         </h2>
-        <div>
-          <p>
-            <a href={this.props.feed.link} target="_blank">{this.props.feed.title}</a>
-          </p>
+        <div className="trend-feed" style={{ width: '50%' }}>
+          <a href={this.props.feed.link} target="_blank"><font color="black">{this.props.feed.title}</font></a>
+          <div>
+            {feed}
+          </div>
         </div>
       </div>
     );
